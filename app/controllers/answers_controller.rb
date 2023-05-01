c2944d585845ec76cb37a9901f3dc09cf079c608
Vicxwild <vicxwild@gmail.com>
2023-05-01 15:35:46 +0300

    feat: add new and create actions, tests to them for answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,6 +5,20 @@ class AnswersController < ApplicationController
     @answer = find_answer
   end
 
+  def new
+    @answer = current_question.answers.new
+  end
+
+  def create
+    @answer = current_question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to question_answer_path(@current_question, @answer)
+    else
+      render :new
+    end
+  end
+
   private
 
   def current_question
@@ -13,5 +27,9 @@ class AnswersController < ApplicationController
 
   def find_answer
     current_question.answers.find(params[:id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
