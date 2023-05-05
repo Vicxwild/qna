2fc0f56f9b9beb1a8a45b4b7e7e434fb85ee669f
Vicxwild <vicxwild@gmail.com>
2023-05-05 20:19:46 +0300

    feat: add feature (with tests) - user can create question

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:user) { create(:user) }
   let(:question) { create(:question) }
 
   describe "GET #index" do
@@ -30,6 +31,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe "GET #new" do
+    before { login(user) }
+
     before { get :new }
 
     it "assigns a new Question to @question" do
@@ -42,6 +45,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe "POST #create" do
+    before { login(user) }
+
     context "with valid attributes" do
       it "saves a new question in the database" do
         expect { post :create, params: {question: attributes_for(:question)} }.to change(Question, :count).by(1)
