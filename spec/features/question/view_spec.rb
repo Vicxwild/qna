f15bce2df3e17268a32955516fff26c72276e559
Vicxwild <vicxwild@gmail.com>
2023-05-08 13:34:26 +0300

    feat: add feature (with tests) - user can view the list of questions

---
+++ spec/features/question/view_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "User can view the list of questions", "
+  In order to get full information
+  As an any user
+  I'd like to be able to view the list of all questions
+" do
+  given!(:questions) { create_list(:question, 3) }
+
+  scenario "User tries to view the list of all questions" do
+    visit questions_path
+
+    expect(page).to have_content questions[0].title
+    expect(page).to have_content questions[1].title
+    expect(page).to have_content questions[2].title
+  end
+end
