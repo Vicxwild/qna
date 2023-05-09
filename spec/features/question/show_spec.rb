bf84c06a990a5155738d4886012438273c5eb41c
Vicxwild <vicxwild@gmail.com>
2023-05-09 12:20:56 +0300

    feat: add feature (with tests) - user can view question with answers

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+feature "User can view question with answers", "
+  In order to get full information from a community
+  As an any type of user
+  I'd loke to be able to view answers along with question
+" do
+  given(:question) { create(:question) }
+  given(:answers) { create_list(:answer, 3, question: question) }
+
+  scenario "User tries to watch question with answers" do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    expect(page).to have_content answers[0].body
+    expect(page).to have_content answers[1].body
+    expect(page).to have_content answers[2].body
+  end
+end
