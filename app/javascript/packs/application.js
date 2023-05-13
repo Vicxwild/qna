c30ea171d39387bd7310b4d9e0fad4d0f79a1cb6
Vicxwild <vicxwild@gmail.com>
2023-05-13 16:29:12 +0300

    feat: install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("jquery")
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
