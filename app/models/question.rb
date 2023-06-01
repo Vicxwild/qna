c236f7d94f758e2f52336e7cfbf494ca9e541acb
Vicxwild <vicxwild@gmail.com>
2023-06-01 12:51:16 +0300

    feat: add story with spec: the user can add one or more files when creating a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@ class Question < ApplicationRecord
   has_many :answers, -> { sort_by_best }, dependent: :destroy
   belongs_to :author, class_name: "User", foreign_key: "author_id"
 
+  has_one_attached :file
+
   validates :title, :body, :author, presence: true
 end
