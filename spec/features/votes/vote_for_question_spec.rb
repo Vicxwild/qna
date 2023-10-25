715f20229e6601d17477ba79aa02c4a1c9d287f1
Vicxwild <vicxwild@gmail.com>
2023-10-25 17:41:25 +0200

    feat: add feature test for like/dislike for question with UI

---
+++ spec/features/votes/vote_for_question_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+feature "User can vote for question", "
+  In order to change the rating of a question
+  As an authenticated user
+  I'd like to be able to vote for question
+" do
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
+
+  describe "Authenticated user", js: true do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario "can vote for question (like)" do
+      within ".question" do
+        find(".like").click
+        wait_for_ajax
+      end
+
+      expect(question.reload.votes_sum).to eq 1
+    end
+
+    scenario "can vote for question (dislike)" do
+      within ".question" do
+        find(".dislike").click
+        wait_for_ajax
+      end
+
+      expect(question.reload.votes_sum).to eq(-1)
+    end
+  end
+
+  describe "Unauthenticated user" do
+    background do
+      visit question_path(question)
+    end
+
+    scenario "can't see the like button" do
+      within ".question" do
+        expect(page).to_not have_selector(".like")
+      end
+    end
+
+    scenario "can't see the dislike button" do
+      within ".question" do
+        expect(page).to_not have_selector(".dislike")
+      end
+    end
+  end
+end
