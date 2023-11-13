999dc52b4daca1518fbe9c0b4449de7993977adb
Vicxwild <vicxwild@gmail.com>
2023-11-13 17:19:40 +0100

    refactor: oauth finder

--- app/services/users/oauth_finder.rb
+++ app/services/users/oauth_finder.rb
@@ -11,10 +11,9 @@ module Users
       return authorization.user if authorization
 
       email = auth.info[:email]
-      user = User.where(email: email).first
+      user = User.find_by(email: email)
 
-      if user
-      else
+      unless user
         password = Devise.friendly_token[0, 20]
         user = User.create!(email: email, password: password, password_confirmation: password)
       end
