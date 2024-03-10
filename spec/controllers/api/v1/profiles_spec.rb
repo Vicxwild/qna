e412fc1b3c12d54d0bd2fa204729b412b2b4de93
Vicxwild <vicxwild@gmail.com>
2024-03-10 10:25:17 +0100

    refactor: move profile shared examples

--- spec/controllers/api/v1/profiles_spec.rb
+++ spec/controllers/api/v1/profiles_spec.rb
@@ -6,22 +6,6 @@ describe "Profiles API", type: :request do
      "ACCEPT" => "application/json"}
   }
 
-  shared_examples "public fields returnable" do
-    it "return all" do
-      %w[id email admin created_at updated_at].each do |attr|
-        expect(response_item[attr]).to eq user_item.send(attr).as_json
-      end
-    end
-  end
-
-  shared_examples "private fields not returnable" do
-    it "doesn't return" do
-      %w[password encrypted_password].each do |attr|
-        expect(response_item).not_to have_key(attr)
-      end
-    end
-  end
-
   describe "GET /api/v1/profiles" do
     let(:user) { create(:user) }
     let(:access_token) { create(:access_token, resource_owner_id: user.id).token }
@@ -39,8 +23,7 @@ describe "Profiles API", type: :request do
 
         before { get api_path, params: {access_token: access_token}, headers: headers }
 
-        it_behaves_like "public fields returnable"
-        it_behaves_like "private fields not returnable"
+        it_behaves_like "fields returnable"
 
         it "have no current user" do
           json_response_body["users"].each do |user_response|
@@ -61,8 +44,7 @@ describe "Profiles API", type: :request do
 
         before { get api_path, params: {access_token: access_token}, headers: headers }
 
-        it_behaves_like "public fields returnable"
-        it_behaves_like "private fields not returnable"
+        it_behaves_like "fields returnable"
       end
     end
   end
